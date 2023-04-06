439c2f2b2b6bc138fdd59eb47c27ab6b8bb2a08b
afaq qazi <afaq2qazi@gmail.com>
2023-04-07 01:02:46 +0500

    added error handling to views function

--- pandasapi/api/views.py
+++ pandasapi/api/views.py
@@ -7,71 +7,111 @@ from django.http import JsonResponse
 import json
 import pandas as pd
 
-
+def getCsvFile(filenameinstorage):
+    try:
+        successful_file = staticfiles_storage.path(filenameinstorage)
+        successful_file_df = pd.read_csv(successful_file)
+        return successful_file_df
+    except FileNotFoundError:
+        print(f'File not found {filenameinstorage}')
+        return None
+    except Exception as e:
+        print(f'Error reading file: {filenameinstorage}')
+        return None
 
 def index(request):
     return render(request, 'api/home.html')
 
 # set api views here for each function
 def get_future_job_trends(request , location):
-    job_trend_file = staticfiles_storage.path('database/job-trends.csv')
-
+    job_trend_file_name = 'database/job-trends.csv'
     results = []
-    
     # Processing
-    job_trends_df = pd.read_csv(job_trend_file)
-    filt = job_trends_df['Location'].str.lower().str.strip().str.replace(" ", "") == location.lower().strip().replace(" ", "")
-    columnsToGet = ['Ranking' , 'Future Job Predictions ']
-    results = job_trends_df[filt][columnsToGet]
-    results = results.to_json(orient='records')
-    results = json.loads(results)
-    return JsonResponse(results , safe=False)
+    job_trends_df = getCsvFile(job_trend_file_name)
+    if job_trends_df is not None:
+
+        filt = job_trends_df['Location'].str.lower().str.strip().str.replace(" ", "") == location.lower().strip().replace(" ", "")
+        columnsToGet = ['Ranking' , 'Future Job Predictions ']
+        results = job_trends_df[filt][columnsToGet]
+        results = results.to_json(orient='records')
+        results = json.loads(results)
+        if len(results) == 0:
+            returnString = f"There is no '{location}' location available. Please make sure the name of the location right."
+            return JsonResponse([{"error":returnString}] , safe=False)
+        return JsonResponse(results , safe=False)
+    else:
+        return JsonResponse([{"error":"an error was encouncered during fetching of data."},] , safe=False)
 
 def get_current_job_trends(request , location):
     results = []
-    job_trend_file = staticfiles_storage.path('database/job-trends.csv')
+    job_trend_file = 'database/job-trends.csv'
     # Processing
-    job_trends_df = pd.read_csv(job_trend_file)
-    filt = job_trends_df['Location'].str.lower().str.strip().str.replace(" ", "") == location.lower().strip().replace(" ", "")
-    columnsToGet = ['Ranking' , 'Jobs Trending Now By Industry' , 'Job Description' , 'Profession Wage Per Area' , 'Living Wage Per Area']    
-    results = job_trends_df[filt][columnsToGet]
-    results = results.to_json(orient='records')
-    results = json.loads(results)
-    
-    return JsonResponse(results , safe=False)
+    job_trends_df = getCsvFile(job_trend_file)
+    if job_trends_df is not None:
+
+        filt = job_trends_df['Location'].str.lower().str.strip().str.replace(" ", "") == location.lower().strip().replace(" ", "")
+        columnsToGet = ['Ranking' , 'Jobs Trending Now By Industry' , 'Job Description' , 'Profession Wage Per Area' , 'Living Wage Per Area']    
+        results = job_trends_df[filt][columnsToGet]
+        results = results.to_json(orient='records')
+        results = json.loads(results)
+        if len(results) == 0:
+            returnString = f"There is no '{location}' location available. Please make sure the name of the location right."
+            return JsonResponse([{"error":returnString}] , safe=False)
+        return JsonResponse(results , safe=False)
+    else:
+        return JsonResponse([{"error":"an error was encouncered during fetching of data."},] , safe=False)
+
 
 def get_available_programs(request , location):
     results = []
-    available_programs_file = staticfiles_storage.path('database/available_programs.csv')
-    available_programs_df = pd.read_csv(available_programs_file)
-    filt = available_programs_df['Location'].str.lower().str.strip().str.replace(" ", "") == location.lower().strip().replace(" ", "")
-    columnsToGet = ['Available Program' , 'Description']
-    results = available_programs_df[filt][columnsToGet]
-    results = results.to_json(orient='records')
-    results = json.loads(results)
+    available_programs_file = 'database/available_programs.csv'
+    available_programs_df = getCsvFile(available_programs_file)
 
-    return JsonResponse(results , safe=False)
+    if available_programs_df is not None:
+
+        filt = available_programs_df['Location'].str.lower().str.strip().str.replace(" ", "") == location.lower().strip().replace(" ", "")
+        columnsToGet = ['Available Program' , 'Description']
+        results = available_programs_df[filt][columnsToGet]
+        results = results.to_json(orient='records')
+        results = json.loads(results)
+        if len(results) == 0:
+            returnString = f"There is no '{location}' location available. Please make sure the name of the location right."
+            return JsonResponse([{"error":returnString}] , safe=False)
+        return JsonResponse(results , safe=False)
+    else:
+        return JsonResponse([{"error":"an error was encouncered during fetching of data."},] , safe=False)
 
 def get_trending_jobs(request , location):
     results = []
-    job_searches_file = staticfiles_storage.path('database/trending-job-searches-per-area.csv')
-    df = pd.read_csv(job_searches_file)
-    filt = df['Location'].str.lower().str.strip().str.replace(" ", "") == location.lower().strip().replace(" ", "")
-    columnsToGet = ['Trending Job Searches ']
-    results = df[filt][columnsToGet]
-    results = results.to_json(orient='records')
-    results = json.loads(results)    
-    
-    return JsonResponse(results , safe=False)
+    job_searches_file = 'database/trending-job-searches-per-area.csv'
+    job_searches_df = getCsvFile(job_searches_file)
+    if job_searches_df is not None:
+
+        filt = job_searches_df['Location'].str.lower().str.strip().str.replace(" ", "") == location.lower().strip().replace(" ", "")
+        columnsToGet = ['Trending Job Searches ']
+        results = job_searches_df[filt][columnsToGet]
+        results = results.to_json(orient='records')
+        results = json.loads(results)    
+        if len(results) == 0:
+            returnString = f"There is no '{location}' location available. Please make sure the name of the location right."
+            return JsonResponse([{"error":returnString}] , safe=False)
+        return JsonResponse(results , safe=False)
+    else:
+        return JsonResponse([{"error":"an error was encouncered during fetching of data."},] , safe=False)
 
 def get_common_language(request , location):
     results = []
-    job_searches_file = staticfiles_storage.path('database/location-information.csv')
-    df = pd.read_csv(job_searches_file)
-    filt = df['Location'].str.lower().str.strip().str.replace(" ", "") == location.lower().strip().replace(" ", "")
-    columnsToGet = ['Language ']
-    results = df[filt][columnsToGet]
-    results = results.to_json(orient='records')
-    results = json.loads(results)    
-    
-    return JsonResponse(results , safe=False)
+    location_file = 'database/location-information.csv'
+    location_file_df = getCsvFile(location_file)
+    if location_file_df is not None:
+        filt = location_file_df['Location'].str.lower().str.strip().str.replace(" ", "") == location.lower().strip().replace(" ", "")
+        columnsToGet = ['Language ']
+        results = location_file_df[filt][columnsToGet]
+        results = results.to_json(orient='records')
+        results = json.loads(results)    
+        if len(results) == 0:
+            returnString = f"There is no '{location}' location available. Please make sure the name of the location right."
+            return JsonResponse([{"error":returnString}] , safe=False)
+        return JsonResponse(results , safe=False)
+    else:
+        return JsonResponse([{"error":"an error was encouncered during fetching of data."},] , safe=False)
